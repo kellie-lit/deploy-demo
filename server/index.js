f0554031d173418f916356ad0602fe5d44dfe5f1
KellieLittlejohn <big_zaddy@Bigs-MacBook-Pro.local>
2022-07-27 16:04:35 -0400

    img again

--- server/index.js
+++ server/index.js
@@ -3,14 +3,14 @@ const path = require("path");
 
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('/', function (_req, res) {
     res.sendFile(path.join(__dirname, '../index.html'))
 });
 
-app.get('/js', (req, res) => {
+app.get('/js', (_req, res) => {
     res.sendFile(path.join(__dirname, './public/main.js'))
   });
-  
+
 const port = process.env.PORT || 4005
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
