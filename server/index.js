64e71c9658c8c2f035ff1d4945325632bad3c030
KellieLittlejohn <big_zaddy@Bigs-MacBook-Pro.local>
2022-07-27 16:20:37 -0400

    changes

--- server/index.js
+++ server/index.js
@@ -3,15 +3,11 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname,`/../public`)))
+app.use(express.static(path.join(__dirname, `/../public`)))
 
 app.get('/', function (_req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 });
-
-app.get('/js', (_req, res) => {
-    res.sendFile(path.join(__dirname, './public/main.js'))
-  });
 
 const port = process.env.PORT || 4005
 app.listen(port, () => {
