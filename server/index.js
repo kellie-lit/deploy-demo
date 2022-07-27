b84b1190e278273ec4e0566124f45092831ae18b
KellieLittlejohn <big_zaddy@Bigs-MacBook-Pro.local>
2022-07-27 15:55:49 -0400

    img styling

--- server/index.js
+++ server/index.js
@@ -7,7 +7,12 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../index.html'))
 });
 
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/main.js'))
+  });
+  
 const port = process.env.PORT || 4005
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 });
+
