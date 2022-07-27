7c2eeeeb2dc90ca87c95806559a9f51c056e66c7
KellieLittlejohn <big_zaddy@Bigs-MacBook-Pro.local>
2022-07-27 15:37:05 -0400

    HELLO

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-const express =require('express');
-const path= require("path");
+const express = require('express');
+const path = require("path");
 
 const app = express();
 
-app.get('/', function(req,res) {
-    res.sendFile(path.join(__dirname, 'index.html'))
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../index.html'))
 });
 
 const port = process.env.PORT || 4005
